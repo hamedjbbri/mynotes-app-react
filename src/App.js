240d3fd3986c654b1e6a25ea6635694f5f4d4479
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-07-28 11:01:03 -0700

    Add new note and some fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import NotesList from "./components/NotesList";
+import AddNote from './components/AddNote';
 
 
 const App = () => {
@@ -27,9 +28,20 @@ const App = () => {
     }
 ]);
 
+const addNote = (text) => {
+   const date = new Date();
+   const newNote = {
+     id: nanoid(),
+     text: text, 
+     date: date.toLocaleDateString()
+   }
+   const newNotes = [...notes, newNote];
+   setNotes(newNotes);
+};
+
 
   return <div className="container">
-        <NotesList notes={notes} />    
+        <NotesList notes={notes} handleAddNote={addNote} />    
     </div>;
 }
 
--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,14 +1,33 @@
-const AddNote = () => {
+import { useState } from 'react';
+
+const AddNote = ({ handleAddNote }) => {
+  const[noteText, setNoteText] = useState(''); 
+
+  const handleChange = (event) => {
+     setNoteText(event.target.value);
+  };
+
+  const handleSaveClick = () => {
+    if(noteText.trim().length > 0){
+        handleAddNote(noteText);
+        setNoteText('');
+      }    
+  };
+
+
   return(
     <div className="note new">
       <textarea 
           cols="10" 
           rows="8" 
-          placeholder="Type to add a note...">
+          placeholder="Type to add a note..."
+          value={noteText}
+          onChange={handleChange}
+          >
       </textarea>
       <div className="note-footer">
         <small>200 Remaining</small>
-        <button className="save">Save</button>
+        <button className="save" onClick={handleSaveClick}>Save</button>
       </div>
     </div>
   );
