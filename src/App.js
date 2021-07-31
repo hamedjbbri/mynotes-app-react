ea2256a42358a4fa39838f1c878d92a3464ebacb
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-07-30 23:04:37 -0700

    New note update and charachter limit

--- src/App.js
+++ src/App.js
@@ -39,10 +39,20 @@ const addNote = (text) => {
    setNotes(newNotes);
 };
 
-
-  return <div className="container">
-        <NotesList notes={notes} handleAddNote={addNote} />    
-    </div>;
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note) => note.id !== id);
+  setNotes(newNotes);
 }
+
+  return (
+    <div className="container">
+        <NotesList 
+          notes={notes} 
+          handleAddNote={addNote} 
+          handleDeleteNote={deleteNote}
+        />    
+    </div>
+    );
+};
 
 export default App;
